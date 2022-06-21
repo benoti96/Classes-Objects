4fd72492fd7ed671fc34e5d56e1bf3348de89e23
benoti96 <107107035+benoti96@users.noreply.github.com>
2022-06-21 14:38:15 +0000

    Initial commit

---
+++ animal.py
@@ -0,0 +1,9 @@
+class Animal:
+    def __init__(self, name, age):
+        # instance variable
+        self.name = name
+        self.age = age
+
+    # instance method
+    def speak(self):
+        return f"My name is {self.name} and I am {self.age} years old"
---
+++ main.py
@@ -0,0 +1,10 @@
+from animal import Animal
+
+# instance / object
+dog = Animal(name='Lizzy', age=3)
+cat = Animal(name='Zorka', age=10)
+bird = Animal(name='Fufu', age=5)
+
+print(dog.speak())
+print(cat.speak())
+print(bird.speak())
